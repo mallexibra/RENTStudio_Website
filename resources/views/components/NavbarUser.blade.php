24904cbd326aee1619dfcaf806af26926b7a42a1
mallexibra <mallexibra@gmail.com>
2023-11-29 13:08:26 +0700

    feat: add layouting

---
+++ resources/views/components/NavbarUser.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-dark p-3 bg-purple shadow-custom">
+    <div class="container">
+        <a class="navbar-brand fs-4 fw-bold" href="#">RENT STUDIO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <div class="d-flex dropdown" style="padding-right: 20px">
+                <span role="button" data-bs-toggle="dropdown" aria-expanded="false"
+                    class="d-block rounded-circle nav-item text-bg-primary" style="width: 42px; height: 42px"></span>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Edit</a></li>
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</nav>
